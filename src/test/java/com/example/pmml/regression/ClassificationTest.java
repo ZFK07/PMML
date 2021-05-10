37477fdf3f8bd97e83f827bffd2179341f9e0591
Zahid Khan <zahid.khan@valebridgetech.com>
2021-05-10 19:15:43 +0530

    Added various normalize methods

--- src/test/java/com/example/pmml/regression/ClassificationTest.java
+++ src/test/java/com/example/pmml/regression/ClassificationTest.java
@@ -2,6 +2,7 @@ package com.example.pmml.regression;
 
 import com.example.pmml.VerificationHelper;
 import org.jpmml.evaluator.EvaluationException;
+import org.jpmml.evaluator.InvalidElementException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -19,12 +20,12 @@ class ClassificationTest {
 
         @Test
         void testPmml_classificationOfSpeed_InvalidCases_ShouldThrowAnException() {
-                Assertions.assertThrows(EvaluationException.class, ()-> VerificationHelper.verify("com/example/pmml/regression_model/classification/classificationOfSpeed_invalidCase.xml"));
+                Assertions.assertThrows(EvaluationException.class, () -> VerificationHelper.verify("com/example/pmml/regression_model/classification/classificationOfSpeed_invalidCase.xml"));
         }
 
         @Test
         void testPmml_classificationOfJobCategory_InvalidCases_ShouldThrowAnException() {
-                Assertions.assertThrows(EvaluationException.class, ()->VerificationHelper.verify("com/example/pmml/regression_model/classification/classificationOfJobCategory_invalidCase.xml"),"Values skilled and professional do not match");
+                Assertions.assertThrows(EvaluationException.class, () -> VerificationHelper.verify("com/example/pmml/regression_model/classification/classificationOfJobCategory_invalidCase.xml"), "Values skilled and professional do not match");
         }
 
 
@@ -37,17 +38,62 @@ class ClassificationTest {
         void testPmml_normalizeMethod_simpleMax() {
                 VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/simplemax.xml");
         }
+
         @Test
         void testPmml_normalizeMethod_none() {
                 VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/none.xml");
         }
+
         @Test
         void testPmml_normalizeMethod_logit() {
                 VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/logit.xml");
         }
 
         @Test
+        void testPmml_normalizeMethod_cloglog_invalid() {
+                Assertions.assertThrows(InvalidElementException.class, () -> VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/cloglog_invalid.xml"), "Element RegressionModel is not valid");
+        }
+
+        @Test
         void testPmml_normalizeMethod_cloglog() {
                 VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/cloglog.xml");
         }
+        @Test
+        void testPmml_normalizeMethod_loglog() {
+                VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/loglog.xml");
+        }
+
+        @Test
+        void testPmml_normalizeMethod_loglog_invalid() {
+                Assertions.assertThrows( EvaluationException.class, ()-> VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/loglog_invalid.xml"));
+        }
+
+        @Test
+        void testPmml_normalizeMethod_cauchit() {
+                VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/cauchit.xml");
+        }
+        @Test
+        void testPmml_normalizeMethod_cauchitInvalid() {
+                Assertions.assertThrows(EvaluationException.class, ()->VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/cauchit_invalid.xml"), "Values fast and slow do not match");
+        }
+
+        @Test
+        void testPmml_normalizeMethod_probit() {
+                VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/probit.xml");
+        }
+        @Test
+        void testPmml_normalizeMethod_probitInvalid() {
+                Assertions.assertThrows(EvaluationException.class, ()->VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/probit_invalid.xml"), "Values fast and slow do not match");
+        }
+
+        @Test
+        void testPmml_normalizeMethod_probitInvalidSyntax() {
+               Assertions.assertThrows(InvalidElementException.class, ()-> VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/probit_invalidSyntax.xml"),"Element RegressionModel is not valid");
+        }
+
+        @Test
+        void testPmml_normalizeMethod_noneInvalid() {
+               Assertions.assertThrows(EvaluationException.class, ()-> VerificationHelper.verify("com/example/pmml/regression_model/classification/normalize_methods/none_invalid.xml"),"Element RegressionModel is not valid");
+        }
+
 }
